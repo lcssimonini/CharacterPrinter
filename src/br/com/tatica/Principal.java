f1991ddd522eebe04e3ea9971acbe8b8b06866ad
lcssi <lcssi@LAPTOP-MFLG9FSU>
2017-01-04 22:01:00 -0200

    inicial

---
+++ src/br/com/tatica/Principal.java
@@ -0,0 +1,45 @@
+package br.com.tatica;
+
+public class Principal {
+	
+	private static String POINTS = ":";
+	private static String HASHSS = "#";
+	
+	public static void main(String[] args) {
+		printImage();
+	}
+	
+	public static void printImage() {
+		int lines = 10;
+		int lineSize = 45;
+		int threshold = 0;
+		int hashIndex = 0;
+		for (int i = 0; i < lines; i++) {
+			if (i < 5) {
+				hashIndex = (22-(3*i));
+			} else {
+				hashIndex += 3;
+			}
+			
+			int hashTimes = (1 + (threshold*3));
+			for (int j = 0; j < lineSize; j++) {
+				if (j == hashIndex) {
+					for (int k = 0; k < hashTimes; k++) {
+						System.out.print(HASHSS);
+						j++;
+					}
+				} else {
+					System.out.print(POINTS);
+				}
+			}
+			
+			if (i < 4) {
+				threshold += 2;
+			} else {
+				threshold -= 2;
+			}
+			
+			System.out.println("");
+		}	
+	}
+}
